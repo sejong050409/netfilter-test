8eb99a8aa9e06e20e75328214564778a7371ab79
sejong050409 <jbs31_1209@js.hs.kr>
2026-04-27 20:53:13 -0400

    Update main.c

--- main.c
+++ main.c
@@ -7,48 +7,94 @@
 #include <libnetfilter_queue/libnetfilter_queue.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include "headers.h"
 
 char *blocked_host;
 
-static u_int32_t get_packet_id(struct nfq_data *nfa) {
-    struct nfqnl_msg_packet_hdr *ph;
-    ph = nfq_get_msg_packet_hdr(nfa);
-    if (ph)
-        return ntohl(ph->packet_id);
-    return 0;
+static u_int32_t print_pkt (struct nfq_data *tb)
+{
+	int id = 0;
+	struct nfqnl_msg_packet_hdr *ph;
+	struct nfqnl_msg_packet_hw *hwph;
+	u_int32_t mark,ifi;
+	int ret;
+	unsigned char *data;
+
+	ph = nfq_get_msg_packet_hdr(tb);
+	if (ph) {
+		id = ntohl(ph->packet_id);
+		printf("hw_protocol=0x%04x hook=%u id=%u ",
+			ntohs(ph->hw_protocol), ph->hook, id);
+	}
+
+	hwph = nfq_get_packet_hw(tb);
+	if (hwph) {
+		int i, hlen = ntohs(hwph->hw_addrlen);
+
+		printf("hw_src_addr=");
+		for (i = 0; i < hlen-1; i++)
+			printf("%02x:", hwph->hw_addr[i]);
+		printf("%02x ", hwph->hw_addr[hlen-1]);
+	}
+
+	mark = nfq_get_nfmark(tb);
+	if (mark)
+		printf("mark=%u ", mark);
+
+	ifi = nfq_get_indev(tb);
+	if (ifi)
+		printf("indev=%u ", ifi);
+
+	ifi = nfq_get_outdev(tb);
+	if (ifi)
+		printf("outdev=%u ", ifi);
+	ifi = nfq_get_physindev(tb);
+	if (ifi)
+		printf("physindev=%u ", ifi);
+
+	ifi = nfq_get_physoutdev(tb);
+	if (ifi)
+		printf("physoutdev=%u ", ifi);
+
+	ret = nfq_get_payload(tb, &data);
+	if (ret >= 0)
+		printf("payload_len=%d\n", ret);
+
+	fputc('\n', stdout);
+
+	return id;
 }
 
 static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
               struct nfq_data *nfa, void *data)
 {
-    u_int32_t id = get_packet_id(nfa);
+    u_int32_t id = print_pkt(nfa);
 
     unsigned char *payload;
     int len = nfq_get_payload(nfa, &payload);
 
     if (len >= 0) {
-        struct iphdr *ip = (struct iphdr *)payload;
-	if (ip->protocol != IPPROTO_TCP)
+        ip_header *ip = (ip_header *)payload;
+	int ip_header_len = (ip->ver_ihl & 0x0F) * 4;
+
+	if (ip->protocol != 6)
             return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
-	int ip_header_len = ip->ihl * 4;
        
-        struct tcphdr *tcp = (struct tcphdr *)(payload + ip_header_len);
-	if (ntohs(tcp->dest) != 80)
+        tcp_header *tcp = (tcp_header *)(payload + ip_header_len);
+	if (ntohs(tcp->dst_port) != 80)
             return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
 
-        int tcp_header_len = tcp->doff * 4;
-        unsigned char *http = payload + ip_header_len + tcp_header_len;
+        int tcp_header_len = ((tcp->offset_reserved >> 4) & 0x0F) * 4;        
+	
+	unsigned char *http = payload + ip_header_len + tcp_header_len;
 
         if (http >= payload + len)
             return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
 
-        if (memcmp(http, "GET", 3) != 0 &&
-            memcmp(http, "POST", 4) != 0)
-            return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
-
-        char *host = strstr((char *)http, "Host: ");
+        char *host = strcasestr((char *)http, "Host:");
         if (host) {
-            host += 6;
+            host += 5;
+	    while (*host == ' ') host++;
 
             char *end = strstr(host, "\r\n");
             if (end) {
@@ -57,6 +103,7 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
 
                 if (host_len < sizeof(domain)) {
                     strncpy(domain, host, host_len);
+		    domain[host_len] = '\0';
 
                     printf("[HTTP Host] %s\n", domain); 
                     if (strcmp(domain, blocked_host) == 0) {
