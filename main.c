0fe6a87a952478f7a70f927a232ca372ff1e3d3f
sejong050409 <jbs31_1209@js.hs.kr>
2026-04-27 21:46:57 -0400

    Update main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include <libnetfilter_queue/libnetfilter_queue.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <errno.h>
 #include "headers.h"
 
 char *blocked_host;
@@ -91,7 +92,7 @@ static int cb(struct nfq_q_handle *qh, struct nfgenmsg *nfmsg,
         if (http >= payload + len)
             return nfq_set_verdict(qh, id, NF_ACCEPT, 0, NULL);
 
-        char *host = strcasestr((char *)http, "Host:");
+        char *host = strstr((char *)http, "Host:");
         if (host) {
             host += 5;
 	    while (*host == ' ') host++;
@@ -132,29 +133,35 @@ int main(int argc, char **argv)
     struct nfq_q_handle *qh;
     int fd;
     int rv;
-    char buf[4096];
+    char buf[4096] __attribute__ ((aligned));
 
+    printf("opening library handle\n");
     h = nfq_open();
     if (!h) {
         fprintf(stderr, "error during nfq_open()\n");
         exit(1);
     }
-
+    
+    printf("unbinding existing nf_queue handler for AF_INET (if any)\n");    
     if (nfq_unbind_pf(h, AF_INET) < 0) {
         fprintf(stderr, "error during nfq_unbind_pf()\n");
+	exit(1);
     }
 
+    printf("binding nfnetlink_queue as nf_queue handler for AF_INET\n");
     if (nfq_bind_pf(h, AF_INET) < 0) {
         fprintf(stderr, "error during nfq_bind_pf()\n");
         exit(1);
     }
 
+    printf("binding this socket to queue '0'\n");    
     qh = nfq_create_queue(h, 0, &cb, NULL);
     if (!qh) {
         fprintf(stderr, "error during nfq_create_queue()\n");
         exit(1);
     }
 
+    printf("setting copy_packet mode\n");
     if (nfq_set_mode(qh, NFQNL_COPY_PACKET, 0xffff) < 0) {
         fprintf(stderr, "can't set packet_copy mode\n");
         exit(1);
@@ -162,12 +169,34 @@ int main(int argc, char **argv)
 
     fd = nfq_fd(h);
 
-    while ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
-        nfq_handle_packet(h, buf, rv);
-    }
+    for (;;) {
+		if ((rv = recv(fd, buf, sizeof(buf), 0)) >= 0) {
+			printf("pkt received\n");
+			nfq_handle_packet(h, buf, rv);
+			continue;
+		}
+		if (rv < 0 && errno == ENOBUFS) {
 
-    nfq_destroy_queue(qh);
-    nfq_close(h);
+			printf("losing packets!\n");
+			continue;
+		}
+		perror("recv failed");
+		break;
+	}
 
-    return 0;
+	printf("unbinding from queue 0\n");
+	nfq_destroy_queue(qh);
+
+#ifdef INSANE
+	/* normally, applications SHOULD NOT issue this command, since
+	 * it detaches other programs/sockets from AF_INET, too ! */
+	printf("unbinding from AF_INET\n");
+	nfq_unbind_pf(h, AF_INET);
+#endif
+
+	printf("closing library handle\n");
+	nfq_close(h);
+
+	exit(0);
 }
+
